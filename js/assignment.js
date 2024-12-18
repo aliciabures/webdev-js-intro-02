d42ed93485d4661218d20c08dd67ef9c01133b3e
Alicia <arosebures@gmail.com>
2024-12-17 22:53:45 -0500

    Completed task two. Added variables and updated functions

--- js/assignment.js
+++ js/assignment.js
@@ -8,28 +8,32 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const resolution = "Go for more walks in the neighborhood"
+const currentYear = 2024
+let willMeetResolution = "Yes"
 
 function updateYear() {
     // Update this function
-
+yearElement.innerText = currentYear
 }
 
 function updateResolution() {
     // Update this function 
-
+resolutionElement.innerText = resolution
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+willMeetResolutionElement.innerText= willMeetResolution
 }
 
 function render() {
     // Update this function
-
+updateResolution()
+updateWillMeetResolution()
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+render()
 })
