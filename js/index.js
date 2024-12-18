550dc10b1562dafb8d9fd3ce4a328e1eabdd0be9
Alicia <arosebures@gmail.com>
2024-12-17 22:36:23 -0500

    Completed task one. Experimented and commented out the code.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,14 @@
 // that are not declared
 "use strict"
 
+const three = 3;
+console.log (three);
+console.log (typeof three);
+
+let flower = "rose";
+console.log (flower);
+console.log (typeof flower);
+
+let isComplete = true
+console.log (isComplete)
+console.log (typeof isComplete)
